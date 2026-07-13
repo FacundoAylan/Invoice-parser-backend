c72ed5f787e3caa13b60b4b159174c8ebc12e8ca
FacundoAylan <facundogsb@gmail.com>
2026-07-13 14:28:06 -0300

    fix: deploy

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,10 +1,16 @@
 import { loadEnvFile } from "node:process";
 
-loadEnvFile();
+try {
+  loadEnvFile();
+} catch (error) {
+  console.log(
+    "Aviso: No se encontró el archivo .env, usando variables del entorno.",
+  );
+}
 
 export const {
   PORT,
   GOOGLEAI_API_KEY,
   GOOGLEAI_MODEL,
-  API_V1 = "/api/v1"
+  API_V1 = "/api/v1",
 } = process.env;
